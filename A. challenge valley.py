83f6a02c2c775c6723ee58de7b26832a18ae1191
obsa <obsukebede2022@gmail.com>
2023-10-14 21:07:54 +0300

    contest

---
+++ A. challenge valley.py
@@ -0,0 +1,23 @@
+def solve():
+    n, k, q = map(int, input().split())
+    a = list(map(int, input().split()))
+    ans = 0
+    len = 0
+    for i in range(n):
+        if a[i] <= q:
+            len += 1
+        else:
+            if len >= k:
+                ans += (len - k + 1) * (len - k + 2) // 2
+            len = 0
+    if len >= k:
+        ans += (len - k + 1) * (len - k + 2) // 2
+    print(ans)
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        solve()
+
+if name == "main":
+    main()
---
+++ B. Ski Resort.py
@@ -0,0 +1,60 @@
+import java.util.HashSet;
+import java.util.*;
+
+import javax.naming.spi.StateFactory;
+import javax.swing.text.Position;
+public class pot{
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        int t2=sc.nextInt();
+
+        while(t2>0){
+
+
+         int n=sc.nextInt();
+         int k=sc.nextInt();
+         int q=sc.nextInt();
+
+         List<Integer> li=new ArrayList<>();
+         int count=0;
+
+         for(int i=0;i<n;i++){
+
+            int tem=sc.nextInt();
+            if(tem<=q){
+                count++;
+                if(i==n-1)
+                li.add(count);
+            }else{
+                li.add(count);
+                count=0;
+            }
+         }
+
+         long ans=0;
+
+         //System.out.println(li);
+         for(int i=0;i<li.size();i++){
+
+            int cons=li.get(i);
+
+            for(int j=1;j<=cons;j++){
+                int plus=j+k;
+                plus--;
+                if(plus<=cons){
+
+                    long ways=cons-plus;
+                    ways++;
+                    ans+=ways;
+                }
+            }
+         }
+         System.out.println(ans);
+            t2--;
+        }
+
+    }
+}
