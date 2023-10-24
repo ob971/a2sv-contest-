ffee81a2c88a4cbd19f3401f35bcf218dddfcde5
obsu kebede <134233644+ob971@users.noreply.github.com>
2023-10-24 18:59:54 +0300

    Create A. Lucky Numbers.py

---
+++ A. Lucky Numbers.py
@@ -0,0 +1,28 @@
+def getLuck(num):
+    numS = sorted(str(num))
+    return int(numS[-1]) - int(numS[0])
+
+def solve():
+    l, r = map(int, input().split())
+
+    lucky = l
+    t = getLuck(l)
+
+    for i in range(l, r+1):
+        luck = getLuck(i)
+
+        if luck > t:
+            t = luck
+            lucky = i
+
+        if t == 9:
+            print(lucky, end="")
+            return
+
+    print(lucky, end="")
+
+t = int(input())
+
+for _ in range(t):
+    solve()
+    print()
