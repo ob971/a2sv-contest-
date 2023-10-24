c823c6d9bb42836f26dee23827b83bf376b973b0
obsu kebede <134233644+ob971@users.noreply.github.com>
2023-10-24 20:08:39 +0300

    Create E WRATH.py

---
+++ E WRATH.py
@@ -0,0 +1,19 @@
+size = int(input())
+strengths = list(map(int, input().split()))
+ 
+killed = 0
+last_killed = size
+ 
+for i in range(size-1, -1, -1):
+    current_kill = max(0, i - strengths[i])
+    if current_kill >= last_killed:
+        continue
+    if i < last_killed:
+        killed += i - current_kill
+    else:
+        killed += last_killed - current_kill
+    if current_kill == 0:
+        break
+    last_killed = min(last_killed, current_kill)
+ 
+print(size - killed)
