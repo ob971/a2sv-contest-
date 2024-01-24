8dba135fbdeb73de4e0a2b81c23956deb64a182c
obsa <obsukebede2022@gmail.com>
2024-01-24 12:00:00 +0300

    upadting

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -1,8 +1,8 @@
 def isPalindrome(self, s: str) -> bool:
-            new=("".join(i for i in s if i.isalum())).lower()
+            new=(""join(i for i in s if i.isalum())).lower()
 
 
-            
+
             return new==new[::-1]
 
             
