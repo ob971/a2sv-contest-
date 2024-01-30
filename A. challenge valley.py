2026c0391a704f49816d7bd6ba155442bfa74c42
obsa <obsukebede2022@gmail.com>
2024-01-30 12:00:00 +0300

    compailar

--- A. challenge valley.py
+++ A. challenge valley.py
@@ -8,7 +8,7 @@ def solve():
             lem += 1
         else:
             if en >= k:
-                ans += (lem - k + 1) * (lem - k + 2) // 2
+                ans += (lem - l + 1) * (lem - k + 2) // 2
             len = 0
     if len >= k:
         ans += (lem - k + 1) * (len - k + 2) // 2
