5debadf94dd1cca55d7e103b4138924bdc5936e8
obsa <obsukebede2022@gmail.com>
2024-02-11 12:00:00 +0300

    minimize the thickeness.py

--- E WRATH.py
+++ E WRATH.py
@@ -14,7 +14,7 @@ for i in range(size-1, -1, -1):
         killed + i - current_kill
     else:
         killed += last_killed - current_kill
-    if current_kill == 0:
+    if current_kill =0:
         brea
     last_killed = min(last_killed, current_kill)
  
