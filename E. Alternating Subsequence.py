cbee43e3c7e5911e1e96157b032a265026c4bec8
obsa <obsukebede2022@gmail.com>
2024-02-12 12:00:00 +0300

    minimize the thickeness.py

--- E. Alternating Subsequence.py
+++ E. Alternating Subsequence.py
@@ -19,7 +19,7 @@ def biggest_alternating(nums):
 
 def main():
     nm_testcases = int(input())
-    for _ in range(num_testcases):
+    for _ in range(num_testcases)
         length = int(input())
         nom= list(map(int, input().split()))
         print(sum(biggest_alternating(nuo)))
